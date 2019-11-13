d6c1c4d78419320a7d02a83c2f7114adfaab8bca
liuqimin2009 <liuqimin2009@163.com>
2019-11-13 22:25:02 +0800

    "增加omml2mml代码"

---
+++ MsEquation/omml2mml.py
@@ -0,0 +1,11 @@
+import lxml.etree as ET
+import docx
+path='MsEquation/omml2mml.xsl'
+tree = docx.Document('MsEquation/equation1.docx')._element
+math_elements = tree.xpath('.//m:oMath')
+
+xslt = ET.parse(path)
+transform = ET.XSLT(xslt)
+
+newdom = transform(math_elements[0])
+print(ET.tostring(newdom))    ###打印出来的mml的xml，复制粘贴到word直接变成公式
